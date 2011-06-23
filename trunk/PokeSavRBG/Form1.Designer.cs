e7c2a24f1fe0d17652d0de7d19f90fe91ba0ba11
Benny <Benny@687f5af1-52ef-49e3-a6a9-6010cea96fef>
2011-06-23 20:35:20 +0000

    Changes to PokeSavRBG: ====================== -Minor optical changes to PokeSavRBG.
    git-svn-id: svn://desire.ath.cx/projects/PokeSavRBG@14 687f5af1-52ef-49e3-a6a9-6010cea96fef

--- trunk/PokeSavRBG/Form1.Designer.cs
+++ trunk/PokeSavRBG/Form1.Designer.cs
@@ -46,7 +46,6 @@
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -226,22 +225,11 @@
             this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
             this.saveAsToolStripMenuItem.Text = "Save as";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(191, 252);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 403);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(574, 127);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -276,7 +264,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
 
